44045b67b32582cba4cb060ba1324ba754143962
Ivan Lednev <feyrune33@yandex.ru>
2026-05-05 23:38:00 +0300

    setting up the environment

---
+++ src/popup/index.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <div style={{ padding: 16, width: 280 }}>
+    <h2>HH Lens</h2>
+    <p>Работает!</p>
+  </div>
+)
